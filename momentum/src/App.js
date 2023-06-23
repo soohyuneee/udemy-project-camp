6f7bfda7c88ee7726a3f08145aaaf8da1dde24e3
soohyuneee <tlstngus95@naver.com>
2023-06-24 00:39:26 +0900

    feat : 배경, 시계 렌더링

---
+++ momentum/src/App.js
@@ -0,0 +1,14 @@
+import 'App.css';
+import Background from 'components/Background';
+import Clock from 'components/Clock';
+
+function App() {
+  return (
+    <>
+      <Background />
+      <Clock />
+    </>
+  );
+}
+
+export default App;
