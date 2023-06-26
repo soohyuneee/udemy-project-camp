cbcfd7dfd89317899bef1fd7a7143ffa762d0be1
soohyuneee <tlstngus95@naver.com>
2023-06-26 19:00:45 +0900

    feat : loading 컴포넌트 추가

---
+++ movie-app/src/components/Loding.js
@@ -0,0 +1,11 @@
+import styles from 'styles/Loading.module.css';
+
+function Loading() {
+  return (
+    <div className={styles.loading}>
+      <h1>Loading...</h1>
+    </div>
+  );
+}
+
+export default Loading;
