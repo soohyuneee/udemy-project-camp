38759f25b50277829e10ee1c1eac3fef288c67ee
soohyuneee <tlstngus95@naver.com>
2023-06-27 16:02:27 +0900

    feat : todolist, weather 컴포넌트 렌더링 및 props 추가

--- momentum/src/App.js
+++ momentum/src/App.js
@@ -2,13 +2,20 @@ import 'App.css';
 import Background from 'components/Background';
 import Clock from 'components/Clock';
 import Login from 'components/Login';
+import TodoList from 'components/TodoList';
+import Weather from 'components/Weather';
 
 function App() {
+  const savedUsername = localStorage.getItem('username');
+  const savedTodos = JSON.parse(localStorage.getItem('todos')) || [];
+
   return (
     <>
       <Background />
       <Clock />
-      <Login />
+      <Login savedUsername={savedUsername} />
+      <TodoList initialTodos={savedTodos} />
+      <Weather />
     </>
   );
 }
